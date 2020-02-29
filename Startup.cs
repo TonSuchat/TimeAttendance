a9810f6a53fd3323d680477345ae8ec103addcf7
TonSuchat <suchat.v26@gmail.com>
2020-02-29 21:17:40 +0700

    Add Swagger

--- Startup.cs
+++ Startup.cs
@@ -1,16 +1,12 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using TimeAttendance.Models;
 
 namespace TimeAttendance
@@ -30,6 +26,14 @@ namespace TimeAttendance
             var conectionString = Configuration.GetConnectionString("dbConnectionString");
             services.AddDbContext<TimeAttendanceContext>(options => options.UseMySQL(conectionString));
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo()
+                {
+                    Title = "TimeAttendance-API",
+                    Version = "v1"
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,7 +44,14 @@ namespace TimeAttendance
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "TimeAttendance V1");
+            });
+
+            // app.UseHttpsRedirection();
 
             app.UseRouting();
 
